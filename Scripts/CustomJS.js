50bdf38b19877a82ff4917b03441e3d9a32570f7
Mack <macknz7@gmail.com>
2020-10-08 10:17:10 -0700

    Sunset ( ) Refactor

--- Scripts/CustomJS.js
+++ Scripts/CustomJS.js
@@ -1,28 +1,26 @@
 ﻿function Sunset(...buildings) {
-    console.log(buildings);
-    let results = ['See'];
-
+    // First building can always see sunset   
+    let results = ['See'];  
     // For each building (excluding the first one)
     for (let i = 1; i < buildings.length; i++) {
+        // assume no building is bigger
         let bigger = false;
         // Check if any building before it is larger
-        for (let j = 0; j < i; j++) {
-            if (buildings[j] >= buildings[i]) {
+        for (let j = 0; j < i; j++) { 
+            // previous greater than current?
+            if (buildings[j] >= buildings[i]) { 
                 // If previous building is bigger
-                bigger = true;
-                break;
+                bigger = true; 
+                // stop checking if bigger is found
+                break; 
             } else {
-                // If previous building is smaller
+                // If previous building is smaller, check next
                 continue;
             }
         }
-        if (bigger) {
-            results.push("Blind");
-        } else {
-            results.push("See");
-        }
+        // If there's a bigger building we're blind, otherwise we can see
+        bigger ? results.push("Blind") : results.push("See");
     }
-
     document.getElementById("result").innerText = results;
 }
 
